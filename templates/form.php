1f90a79789f28d40a8bca0f269c9fa479d85ac78
alct <git@loconte.be>
2012-10-06 04:21:57 +0200

    Improved name and firstname handling

--- templates/form.php
+++ templates/form.php
@@ -25,10 +25,12 @@ if ($_SERVER['HTTPS']
  && isset($_SERVER['SSL_CLIENT_S_DN_S'])
  && isset($_SERVER['SSL_CLIENT_S_DN']))
 {
-  $firstnames = explode(" ", $_SERVER['SSL_CLIENT_S_DN_G']);
-  $firstname = $firstnames[0];
-  $name      = $_SERVER['SSL_CLIENT_S_DN_S'];
-  $sex       = substr($_SERVER['SSL_CLIENT_S_DN'], -3, 1) % 2; // 1 man; 0 woman
+  $firstnames = explode(' ', $_SERVER['SSL_CLIENT_S_DN_G']);
+  $firstname  = $firstnames[0];
+
+  $name       = $_SERVER['SSL_CLIENT_S_DN_S'];
+
+  $sex        = substr($_SERVER['SSL_CLIENT_S_DN'], -3, 1) % 2; // 1 man; 0 woman
 
   // if this very form was submitted
   if (isset($_POST['form'])
